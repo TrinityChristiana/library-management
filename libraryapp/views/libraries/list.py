abd02dd70656647a89b28fd67f74e61ceb5c2d41
Trinity Terry <trinityterry@Trinitys-MacBook-Pro.local>
2020-05-03 23:12:51 -0500

    added views and template chapter

---
+++ libraryapp/views/libraries/list.py
@@ -0,0 +1,43 @@
+import sqlite3
+from django.shortcuts import render
+from libraryapp.models import Library
+from ..connection import Connection
+
+
+def list_libraries(request):
+    if request.method == 'GET':
+        with sqlite3.connect(Connection.db_path) as conn:
+            conn.row_factory = sqlite3.Row
+
+            db_cursor = conn.cursor()
+
+            db_cursor.execute("""
+            select 
+                ll.id, 
+                ll.title, 
+                ll.address 
+            from libraryapp_library ll
+            """)
+
+            all_libraries = []
+            dataset = db_cursor.fetchall()
+
+            for row in dataset:
+                lib = Library()
+                lib.id = row["id"]
+                lib.title = row['title']
+                lib.address = row['address']
+                all_libraries.append(lib)
+            
+
+        template = 'libraries/list.html'
+
+        context = {
+            'all_libraries': all_libraries
+        }
+
+        return render(request, template, context)
+
+            
+
+
