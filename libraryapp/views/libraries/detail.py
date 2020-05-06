b075f616437bae66a288e6fe53f70a92b9bb07d3
Trinity Terry <trinityterry@Trinitys-MacBook-Pro.local>
2020-05-06 12:33:13 -0500

    finished extenging template chapter

--- libraryapp/views/libraries/detail.py
+++ libraryapp/views/libraries/detail.py
@@ -6,13 +6,46 @@ from libraryapp.models import Library, model_factory
 from ..connection import Connection
 from ...utils import get_library
 
+
 @login_required
 def library_detail(request, library_id):
     if request.method == "GET":
-       library = get_library(library_id)
-       template = "libraries/detail.html"
-       context = {
-           'library': library
-       }
-       
-       return render(request, template, context)
+        library = get_library(library_id)
+        template = "libraries/detail.html"
+        context = {
+            'library': library
+        }
+
+        return render(request, template, context)
+
+    if request.method == "POST":
+        form_data = request.POST
+            
+        if "actual_method" in form_data and form_data["actual_method"] == "Delete":
+            print("DELETING ************")
+            with sqlite3.connect(Connection.db_path) as conn:
+                db_cursor = conn.cursor()
+
+                db_cursor.execute("""
+                DELETE from libraryapp_library
+                WHERE id = ?
+                """, (library_id, ))
+
+            return redirect(reverse("libraryapp:libraries"))
+
+        if "actual_method" in form_data and form_data['actual_method'] == "PUT":
+            print("UPDATING ************")
+            with sqlite3.connect(Connection.db_path) as conn:
+                db_cursor = conn.cursor()
+
+                db_cursor.execute("""
+                UPDATE libraryapp_library
+                SET title = ?,
+                    address = ?
+                WHERE id = ?; 
+                """, (
+                    form_data["name"],
+                    form_data["address"],
+                    library_id
+                ))
+            return redirect(reverse("libraryapp:libraries"))
--- libraryapp/views/libraries/form.py
+++ libraryapp/views/libraries/form.py
@@ -3,10 +3,20 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from libraryapp.models import Book, Library, model_factory
 from ..connection import Connection
+from ...utils import get_library
 
 @login_required
 def library_form(request):
     if request.method == "GET":
         template = "libraries/form.html"
         context = {}
+        return render(request, template, context)
+
+@login_required
+def library_edit_form(request, library_id):
+    # print(request.method, "***********")
+    if request.method == "GET":
+        library = get_library(library_id)
+        template = "libraries/form.html"
+        context = {'library': library}
         return render(request, template, context)
